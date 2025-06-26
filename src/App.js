9f396c402dd736ef33b274f60d69a61517fa1fcb
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-26 12:08:20 -0300

    Adicionando handleClick

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  
+  return (
+
+    <button className="square" onClick={onSquareClick}>
+
+      {value}
+
+    </button>
+
+  );
+
+}
+
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+
+    const nextSquares = squares.slice();
+
+    nextSquares[i] = 'X';
+
+    setSquares(nextSquares);
+
+  }
 
   return (
 
     <>
 
-      <div className='board-row'>
+      <div className="board-row">
 
-        <button className="square">1</button>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 
-        <button className="square">2</button>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
 
-        <button className="square">3</button>
-
-      </div>
-
-      <div className='board-row'>
-
-        <button className="square">4</button>
-
-        <button className="square">5</button>
-
-        <button className="square">6</button>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 
       </div>
 
-      <div className='board-row'>
+      <div className="board-row">
 
-        <button className="square">7</button>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
 
-        <button className="square">8</button>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
 
-        <button className="square">9</button>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 
       </div>
-    
-    
+
+      <div className="board-row">
+
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+
+      </div>
+
     </>
 
   );
